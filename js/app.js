e50908347d949cb032c72c4cc8886d09d9634075
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2020-05-12 19:22:28 +1000

    app.js updated

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const search = document.querySelector('#search input');
 search.addEventListener('input', () => {
     let input = search.value.toLowerCase();
     let text = input.charAt(0).toUpperCase() + input.slice(1);
-    if (text.length > 5) {
+    if (text.length >= 2) {
         //search API
         ui.getSuggestions(text);
         console.log(text);
